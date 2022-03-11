b4fe058c882bac02c7e7159b08322f52200cd3cc
VietNK1 <viet1393c@gmail.com>
2022-03-11 09:44:57 +0700

    Add log start/end

--- Test/Test.java
+++ Test/Test.java
@@ -420,6 +420,7 @@ public class CodeConverter {
   }
 
   public static void main(String[] args) {
+	System.out.print("Convert start");
     // Map map = halfToFullMap;
     Map map = fullToHalfMap;
     java.util.Enumeration iter = ((Properties) map).keys();
@@ -441,6 +442,9 @@ public class CodeConverter {
     } catch (Exception ex) {
       Logger.warn(CodeConverter.class, ex);
     }
+	System.out.print("Add Test commit");
+	
+	System.out.print("Convert end");
   }
 
   public static String encodeHTML(String str) {
---
+++ Test/Test2.java
@@ -0,0 +1,6 @@
+  public static void main(String[] args) {
+	System.out.print("Convert start");
+	System.out.print("Add new file test");
+	
+	System.out.print("Convert end");
+  }
